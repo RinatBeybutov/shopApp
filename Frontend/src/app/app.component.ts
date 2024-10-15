02e06e219bb76cabe636f17cd7e6e43db14fa0e2
Ринат <ignat934@gmail.com>
2024-10-15 13:44:56 +0300

    первичный коммит

---
+++ Frontend/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {CategoriesComponent} from "./categories/categories.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CategoriesComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'frontend';
+}
