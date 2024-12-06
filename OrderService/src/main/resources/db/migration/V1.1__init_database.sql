db67f7381a36f6d981e3fc9f575ff74b2414ddd0
Ринат <ignat934@gmail.com>
2024-12-07 02:57:40 +0300

    7: Добавлены миграции для OrderService

--- OrderService/src/main/resources/db/migration/V1.1__init_database.sql
+++ OrderService/src/main/resources/db/migration/V1.1__init_database.sql
@@ -8,8 +8,9 @@ create schema order_service;
 --
 -- Создание таблицы категорий
 --
-create table order_service.categories(
-    id serial unique,
+create table order_service.categories
+(
+    id   serial unique,
     uuid uuid,
     name text
 );
@@ -17,20 +18,22 @@ create table order_service.categories(
 --
 -- Создание таблицы заказов
 --
-create table order_service.orders(
-    id serial unique,
-    uuid uuid,
+create table order_service.orders
+(
+    id         serial unique,
+    uuid       uuid,
     created_at date,
-    user_uuid uuid
+    user_uuid  uuid
 );
 
 --
 -- Создание таблицы продуктов
 --
-create table order_service.products(
-    id serial unique,
-    uuid uuid,
-    name text,
+create table order_service.products
+(
+    id          serial unique,
+    uuid        uuid,
+    name        text,
     category_id integer,
     foreign key (category_id) references order_service.categories (id)
 );
@@ -38,10 +41,11 @@ create table order_service.products(
 --
 -- Создание таблицы связи продукт - заказ
 --
-create table order_service.product_to_order(
-    id serial unique,
-    product_id integer,
-    order_id integer,
+create table order_service.product_to_order
+(
+    id            serial unique,
+    product_id    integer,
+    order_id      integer,
     product_count integer,
     foreign key (product_id) references order_service.products (id),
     foreign key (order_id) references order_service.orders (id)
