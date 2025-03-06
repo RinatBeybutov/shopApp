c870d7c032775013548156eaa837e8e60fbb39b0
Ринат <ignat934@gmail.com>
2025-03-06 15:52:43 +0300

    10: добавлены настройки редиса

--- MetricsService/src/main/java/com/petProject/MetricsService/config/RedisConfig.java
+++ MetricsService/src/main/java/com/petProject/MetricsService/config/RedisConfig.java
@@ -4,16 +4,25 @@ import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedisConfig {
 
+    @Value("${redis.host:localhost}")
+    private String REDIS_HOST;
+
+    @Value("${redis.port:6379}")
+    private String REDIS_PORT;
+
+    private String REDIS_URL = "redis://%s:%s";
+
     @Bean(destroyMethod="shutdown")
     public RedissonClient redisson() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://localhost:6379");
+        config.useSingleServer().setAddress(REDIS_URL.formatted(REDIS_HOST, REDIS_PORT));
         config.setCodec(new JsonJacksonCodec());
         return Redisson.create(config);
     }
